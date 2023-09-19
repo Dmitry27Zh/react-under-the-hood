aa25421e1da88de258de5df269e8a6e1038e4144
Dmitry <dmitry27zh@gmail.com>
2023-09-19 19:38:59 +0300

    useCallback dependencies, closure

--- src/App.js
+++ src/App.js
@@ -5,15 +5,22 @@ import ParagraphOutput from './components/Output/ParagraphOutput/ParagraphOutput
 
 function App() {
   const [isParagraphShown, setIsParagraphShown] = useState(false)
+  const [isTogglingActivated, setIsTogglingActivated] = useState(false)
   const toggleParagraph = useCallback(() => {
-    setIsParagraphShown((prevState) => !prevState)
-  }, [])
+    if (isTogglingActivated) {
+      setIsParagraphShown((prevState) => !prevState)
+    }
+  }, [isTogglingActivated])
+  const activateToggling = () => {
+    setIsTogglingActivated(true)
+  }
   console.log('Re-evaluation: App')
 
   return (
     <div className="app">
       <h1>React под Капотом</h1>
-      {<ParagraphOutput isShown={false} />}
+      {<ParagraphOutput isShown={isParagraphShown} />}
+      <Button onClick={activateToggling}>Activate toggle</Button>
       <Button onClick={toggleParagraph}>Toggle Paragraph</Button>
     </div>
   )
